e78fdad83b089d23c7dbaefdb65c721116004c96
EnshaednHiker <nbingham09@gmail.com>
2017-06-29 21:01:19 -0500

    CI config update

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,17 +1,17 @@
 require ('dotenv').config();
 
-const selenium = require('selenium-download');
-const path = require('path');
-const BINPATH = './node_modules/nightwatch/bin/'; 
+// const selenium = require('selenium-download');
+// const path = require('path');
+// const BINPATH = './node_modules/nightwatch/bin/'; 
 
-require('fs').stat(BINPATH + 'selenium.jar', function (err, stat) { // got it?
-  if (err || !stat || stat.size < 1) {
-    require('selenium-download').ensure(BINPATH, function(error) {
-      if (error) throw new Error(error); // no point continuing so exit!
-      console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
-    });
-  }
-});
+// require('fs').stat(BINPATH + 'selenium.jar', function (err, stat) { // got it?
+//   if (err || !stat || stat.size < 1) {
+//     require('selenium-download').ensure(BINPATH, function(error) {
+//       if (error) throw new Error(error); // no point continuing so exit!
+//       console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
+//     });
+//   }
+// });
 
 const config = {
     "src_folders" : ["tests/e2e"],
@@ -23,12 +23,12 @@ const config = {
 
     "selenium" : {
       "start_process" : true,
-      "server_path" : "./node_modules/nightwatch/bin/selenium.jar",
+      "server_path" : "./node_modules\selenium-server-standalone-jar\jar\selenium-server-standalone-3.4.0.jar",
       "log_path" : "",
       "port" : 4444,
       "host": "127.0.0.1",
       "cli_args" : {
-        "webdriver.chrome.driver" : "./node_modules/nightwatch/bin/chromedriver"
+        "webdriver.chrome.driver" : "./node_modules/chromedriver/bin/chromedriver"
       }
     },
     "test_workers" : {"enabled" : true, "workers" : "auto"},
